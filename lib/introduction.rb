7c88ea30df10baf156239fefe15c9208804fe738
David Molina <davidmolina2810@gmail.com>
2020-08-08 18:25:00 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -12,6 +12,12 @@ def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
 end
 
+#mehtod that will output "Hi my name is #{name} and I am learning to prorgram in #{language}.\n" 
+# where language will default to "Ruby"
+def introduction_with_language_optional(name, language="Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
+end
+
 
 
 
